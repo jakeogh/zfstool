615f672886fda22fad8db747f6ae161ec0664174
jakeogh <github.com@v6y.net>
2023-11-13 15:38:01 -0700

    auto-commit

--- zfstool/__init__.py
+++ zfstool/__init__.py
@@ -5,3 +5,4 @@ from .zfstool import create_zfs_pool
 from .zfstool import write_zfs_root_filesystem_on_devices
 from .zfstool import zfs_check_mountpoints
 from .zfstool import zfs_set_sharenfs
+from .zfstool import zpool_is_imported
