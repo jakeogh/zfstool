f6409aa114d29ae704872143484feeec8ef4e042
jakeogh <github.com@v6y.net>
2026-07-21 17:23:31 -0700

    update

--- zfstool/zfstool.py
+++ zfstool/zfstool.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=missing-param-doc               # [W9015]
-# pylint: disable=missing-module-docstring        # [C0114]
-# pylint: disable=fixme                           # [W0511] todo encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive(!)
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-many-public-methods         # [R0904]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 from __future__ import annotations
 
 import os
@@ -151,9 +131,24 @@ def zfs_check_mountpoints(
     ),
 )
 @click.option("--force", is_flag=True, required=False)
-@click.option("--raid", is_flag=False, required=True, type=click.Choice(RAID_LIST))
-@click.option("--raid-group-size", is_flag=False, required=True, type=int)
-@click.option("--pool-name", is_flag=False, required=True, type=str)
+@click.option(
+    "--raid",
+    is_flag=False,
+    required=True,
+    type=click.Choice(RAID_LIST),
+)
+@click.option(
+    "--raid-group-size",
+    is_flag=False,
+    required=True,
+    type=int,
+)
+@click.option(
+    "--pool-name",
+    is_flag=False,
+    required=True,
+    type=str,
+)
 @click.option(
     "--mount-point",
     is_flag=False,
@@ -316,10 +311,31 @@ def write_zfs_root_filesystem_on_devices(
 @click.option("--force", is_flag=True, required=False)
 @click.option("--simulate", is_flag=True, required=False)
 @click.option("--skip-checks", is_flag=True, required=False)
-@click.option("--raid", is_flag=False, required=True, type=click.Choice(RAID_LIST))
-@click.option("--raid-group-size", is_flag=False, required=True, type=int)
-@click.option("--pool-name", is_flag=False, required=True, type=str)
-@click.option("--ashift", is_flag=False, required=False, type=int, help=ASHIFT_HELP)
+@click.option(
+    "--raid",
+    is_flag=False,
+    required=True,
+    type=click.Choice(RAID_LIST),
+)
+@click.option(
+    "--raid-group-size",
+    is_flag=False,
+    required=True,
+    type=int,
+)
+@click.option(
+    "--pool-name",
+    is_flag=False,
+    required=True,
+    type=str,
+)
+@click.option(
+    "--ashift",
+    is_flag=False,
+    required=False,
+    type=int,
+    help=ASHIFT_HELP,
+)
 @click.option("--encrypt", is_flag=True)
 @click_add_options(click_global_options)
 @click.pass_context
